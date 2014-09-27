0817d0e8d05a92f43267a056181bf36f9eacdf94
LeaX_XIV <LeaX_XIV@3d28d3d4-525b-4eb2-aefd-b6ecbc2f8262>
2014-09-27 15:37:37 +0000

    Riscritto completamente l'algoritmo di passaggio tra stanza a stanza per permettere l'utilizzo di porte chiuse e chiavi. Occorre testare ulteriormente.

--- TextualAdventure/src/it/univpm/deit/semedia/gameclasses/rooms/Key.java
+++ TextualAdventure/src/it/univpm/deit/semedia/gameclasses/rooms/Key.java
@@ -1,5 +1,57 @@
 package it.univpm.deit.semedia.gameclasses.rooms;
 
-public class Key {
+import it.univpm.deit.semedia.gameclasses.GameObject;
+import it.univpm.deit.semedia.gameclasses.persons.Person;
+
+import java.security.InvalidParameterException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Random;
+
+public class Key extends GameObject{
+	
+	private ArrayList<Byte> denti;
+	
+	public Key() {
+		super("chiave");
+		denti = new ArrayList<Byte>();
+		Random r = new Random();
+		for (int i = 0; i < 4; i++) {
+			denti.add((byte) r.nextInt(256));			
+		}
+	}
+	
+	public Key(ArrayList<Byte> denti) {
+		super("chiave");
+		if(denti != null) {
+			if(denti.size() == 4) {
+				this.denti = denti;
+				return;
+			}
+		}
+		
+		throw new InvalidParameterException();
+	}
+	
+	public ArrayList<Byte> getCode() {
+		return this.denti;
+	}
+	
+	@Override
+	public String use(Person who) {
+		Room room = (Room) who.getContainedIn();
+		if(room.hasClosedDoor()) {
+			ArrayList<Door> closed = room.getClosedDoor();
+			Iterator<Door> it = closed.iterator();
+			while(it.hasNext()) {
+				Door d = it.next();
+				if(d.unlock(this)) {
+					return "Hai aperto la porta a " + Door.getKeyByValue(d.getOtherEnd(room), d.getRooms().indexOf(room)==0? d.getRooms().get(1):d.getRooms().get(0));
+				}
+			}
+		}
+		
+		return "Nessuna porta da aprire con questa chiave.";
+	}
 
 }
--- TextualAdventure/src/it/univpm/deit/semedia/gameclasses/rooms/Lock.java
+++ TextualAdventure/src/it/univpm/deit/semedia/gameclasses/rooms/Lock.java
@@ -1,5 +1,56 @@
 package it.univpm.deit.semedia.gameclasses.rooms;
 
+import java.security.InvalidParameterException;
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Lock {
+
+	private ArrayList<Byte> cilindro;
+	private boolean open;
+
+	public Lock() {
+		open = false;
+		cilindro = new ArrayList<Byte>();
+		Random r = new Random();
+		for (int i = 0; i < 4; i++) {
+			cilindro.add((byte) r.nextInt(256));			
+		}
+	}
+
+	public Lock(ArrayList<Byte> cilindro) {
+		this.open = false;
+		if(cilindro != null) {
+			if(cilindro.size() == 4) {
+				this.cilindro = cilindro;
+				return;
+			}
+		}
+
+		throw new InvalidParameterException();
+	}
+
+	public ArrayList<Byte> getCode() {
+		return this.cilindro;
+	}
+
+	public boolean isOpen() {
+		return this.open;
+	}
+
+	public void open() {
+		open = true;
+	}
+
+	public boolean unlock(Key key) {
+
+		if(!open) {
+			if(key.getCode().equals(this.getCode())) {
+				open = true;
+			}
+		}
+
+		return open;
+	}
 
 }
